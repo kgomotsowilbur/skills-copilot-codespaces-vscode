8cdc9f00382085dedfef6f3d62372459eca1f3a5
kgomotsowilbur <127843021+kgomotsowilbur@users.noreply.github.com>
2024-06-17 03:38:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMembers() {
+    const members = [
+        {
+            name: 'John Doe',
+            age: 25,
+            skills: ['HTML', 'CSS', 'JS']
+        },
+        {
+            name: 'Jane Doe',
+            age: 32,
+            skills: ['HTML', 'CSS', 'JS', 'React']
+        },
+        {
+            name: 'Jim Doe',
+            age: 25,
+            skills: ['HTML', 'CSS', 'JS']
+        },
+    ];
+    return members;
+}
